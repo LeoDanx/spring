771b467fa03ed8a2ef0d272033ef65ece05b4dfb
angelehl <secahl02@gmail.com>
2023-01-31 16:45:46 -0600

    src/test/java/com/angelehl/springboot/utilizandospringt/UtilizandoSpringApplicationT

---
+++ src/main/java/com/angelehl/springboot/utilizandospring/UtilizandoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.angelehl.springboot.utilizandospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilizandoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilizandoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelehl/springboot/utilizandospring/UtilizandoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelehl.springboot.utilizandospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilizandoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
